82df047957463d02e52e667b7d8e14f06da463f7
Matthew Todd <matthew.todd@gmail.com>
2009-01-15 11:35:20 +0300

    Anniversaries.

--- lib/events/person.rb
+++ lib/events/person.rb
@@ -1,7 +1,8 @@
 module Events
   class Person
     def initialize(attributes)
-      @name          = "#{attributes['first_name']} #{attributes['last_name']}"
+      @first_name    = attributes['first_name']
+      @last_name     = attributes['last_name']
       @birthday      = recurring_date(attributes['birthday'])
       @related_names = attributes['related_names']
       @other_dates   = recurring_date(attributes['other_dates'])
@@ -13,19 +14,68 @@ module Events
       if @birthday
         next_birthday = @birthday.next_occurrence(date)
         if next_birthday.days_until < 7
-          upcoming << "#{@name} turns #{next_birthday.years_since} in #{next_birthday.days_until} days. (#{next_birthday.strftime('%A, %B %e')})".gsub(/\s+/, ' ')
+          upcoming << format(":name turns :age :in_days. (:date)", next_birthday)
         end
       end
 
       if @other_dates['anniversary']
         next_anniversary = @other_dates['anniversary'].next_occurrence(date)
-        upcoming << "#{@name}'s #{next_anniversary.years_since} anniversary is in #{next_anniversary.days_until} days. (#{next_anniversary.strftime('%A, %B %e')})".gsub(/\s+/, ' ')
+        upcoming << format(":name_with_spouse's :ordinal_age anniversary is :in_days. (:date)", next_anniversary)
       end
 
       upcoming
     end
 
     private
+
+    def format(string, recurring_date)
+      string.sub! /:name_with_spouse/, name_with_spouse
+      string.sub! /:name/, name
+      string.sub! /:age/, recurring_date.years_since.to_s
+      string.sub! /:ordinal_age/, ordinalize(recurring_date.years_since)
+      string.sub! /:in_days/, in_days(recurring_date)
+      string.sub! /:date/, recurring_date.strftime('%A, %B %e')
+      string.strip!
+      string.gsub!(/\s+/, ' ')
+      string
+    end
+
+    def name
+      "#{@first_name} #{@last_name}"
+    end
+
+    def name_with_spouse
+      if spouse_name = @related_names['spouse']
+        if spouse_name =~ / #{@last_name}$/
+          [@first_name, spouse_name.sub(/ #{@last_name}$/, '')].sort.join(' & ').concat(' ').concat(@last_name)
+        else
+          [name, spouse_name].sort.join(' & ')
+        end
+      else
+        name
+      end
+    end
+
+    def ordinalize(number)
+      if (11..13).include?(number.to_i % 100)
+        "#{number}th"
+      else
+        case number.to_i % 10
+          when 1; "#{number}st"
+          when 2; "#{number}nd"
+          when 3; "#{number}rd"
+          else    "#{number}th"
+        end
+      end
+    end
+
+    def in_days(recurring_date)
+      case in_days = recurring_date.days_until
+        when 0; "TODAY"
+        when 1; "tomorrow"
+        else    "in #{in_days} days"
+      end
+    end
 
     def recurring_date(date)
       case date
